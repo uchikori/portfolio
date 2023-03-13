05bc4ee8dd84f19a0ab0193195fd73508a815884
uchikori <ucchii.wa@gmail.com>
2023-03-13 18:19:51 +0900

    ♻️

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,26 @@ import { Slide } from "../components/TopPage/Slide";
 import { Layout } from "../components/Layout";
 import { Sns } from "../components/global/Sns";
 import { ScrollLead } from "../components/global/ScrollLead";
+import { useEffect } from "react";
 
 export default function Home() {
   const slides = useRef(null); //slides要素の取得
 
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      let vh = window.innerHeight;
+      slides.current.style = `height: ${vh}px;`;
+    });
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight;
+      slides.current.style = `height: ${vh}px;`;
+    });
+  }, []);
+
   return (
     <>
       <Layout>
-        <div ref={slides} className="slides" style={{ height: "100svh" }}>
+        <div ref={slides} className="slides">
           <div className="slides-nav">
             <nav className="slides-nav__nav">
               <span className="page-current">01</span>
