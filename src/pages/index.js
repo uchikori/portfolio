a9a5ec9a77c6116b44ce4c91f5b791464bdbe882
uchikori <ucchii.wa@gmail.com>
2023-03-19 11:13:55 +0900

    refact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,8 @@
 import * as React from "react";
-
-import { useRef } from "react";
 import { Slide } from "../components/TopPage/Slide";
 import { Layout } from "../components/Layout";
-import { Sns } from "../components/global/Sns";
-import { ScrollLead } from "../components/global/ScrollLead";
-import { useEffect } from "react";
-import { useState } from "react";
 
 export default function Home() {
-  const slides = useRef(null); //slides要素の取得
 
   return (
     <>
