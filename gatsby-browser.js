343c1ed41481f17f0963fb88818cc08d262b1584
uchikori <ucchii.wa@gmail.com>
2024-06-13 21:29:08 +0900

    🚧

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 import "/src/components/styles/style.scss";
-import "@fontsource/orbitron/400.css";
 import "@fontsource/orbitron/500.css";
 import "@fontsource/orbitron/600.css";
 import "@fontsource/orbitron/700.css";
-import "@fontsource/orbitron/800.css";
 import "@fontsource/orbitron/900.css";
+import "@fontsource/noto-sans-jp/500.css";
+import "@fontsource/noto-sans-jp/700.css";
+import "@fontsource/noto-sans-jp/900.css";
