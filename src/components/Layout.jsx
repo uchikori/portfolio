37ef07791b935d21bdbf6fca0b8fd84873fa141c
uchikori <ucchii.wa@gmail.com>
2025-08-18 00:25:59 +0900

    test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Load } from "./global/Loading";
 import { GlobalMenu } from "./global/Menu/GlobalMenu";
 import { MouseCursor } from "./global/MouseCursor";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const Layout = (props) => {
   const { children, hasLoadingObj } = props;
@@ -14,7 +13,6 @@ export const Layout = (props) => {
       <div className="container">
         <GlobalMenu />
         {children}
-        <SpeedInsights />
       </div>
     </>
   );
